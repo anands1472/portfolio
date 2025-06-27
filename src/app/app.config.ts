e9602e9807d75fc1b57ba91f0ed48e7d35691df3
Pilli <anand.pilli@capgemini.com>
2025-06-27 16:13:42 -0500

    latest

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,19 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    }),
     provideZonelessChangeDetection(),
     provideRouter(routes)
   ]
